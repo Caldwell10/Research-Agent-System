d267971569377d914c13d128e64ab249156666f2
Caldwell10 <caldwell.kibe@strathmore.edu>
2025-08-31 02:45:51 +0300

    Optimized agent workflow and system cleanup

--- rag/__init__.py
+++ rag/__init__.py
@@ -4,6 +4,6 @@ RAG (Retrieval-Augmented Generation) package for research paper Q&A
 
 from .text_processor import TextProcessor, TextChunk
 from .embedding_service import EmbeddingService
-from .vector_store import VectorStore
+from .s3_vector_store import S3VectorStore
 
-__all__ = ['TextProcessor', 'TextChunk', 'EmbeddingService', 'VectorStore']
+__all__ = ['TextProcessor', 'TextChunk', 'EmbeddingService', 'S3VectorStore']
